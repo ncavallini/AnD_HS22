db34648041b6028d0a4aa13813a8a00188413892
Niccolò Cavallini <73892369+ncavallini@users.noreply.github.com>
2023-01-11 13:39:47 +0100

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -8,6 +8,25 @@ public class Sorting {
        }
     }
     
+  public static void selectionSort(int[] input) {
+        for(int i=0; i < input.length; i++) {
+            int minIndex = minIndex(input, i);
+            swap(i, minIndex, input);
+        }
+    }
+    
+  
+    // Returns the index of the minimum element in a[i...n-1]
+    private static int minIndex(int[] a, int i) {
+        int min = Integer.MAX_VALUE;
+        int minIndex = i;
+        for(int j=i; j < a.length; j++) {
+            min = Math.min(min, a[j]);
+            if(min == a[j]) minIndex = j;
+        }
+        return minIndex;
+    }
+  
     private static void swap(int i, int j, int[] a) {
         int tmp = a[i];
         a[i] = a[j];
